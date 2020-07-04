5978f175afb72bd8c7238dd765ee382e3482fdd0
hdpoliveira <hdpoliveira@gmail.com>
2020-07-03 21:58:07 -0300

    Part of #95: Create infrastructure for testing

---
+++ src/test/suite/dummy.test.ts
@@ -0,0 +1,22 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Ben Crowl. All rights reserved.
+ *  Original Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as assert from 'assert';
+
+// You can import and use all API from the 'vscode' module
+// as well as import your extension to test it
+import * as vscode from 'vscode';
+// import * as myExtension from '../../extension';
+
+suite('Dummy Test Suite', () => {
+	vscode.window.showInformationMessage('Start all tests.');
+
+	test('Dummy test', () => {
+		assert.equal(-1, [1, 2, 3].indexOf(5));
+		assert.equal(-1, [1, 2, 3].indexOf(0));
+		assert.equal(0, [1, 2, 3].indexOf(1));
+	});
+});
